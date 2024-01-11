2abef55cbe2767c480f56b8b4619c4708d10bf6e
RedBarz <barzcorp.fx@gmail.com>
2024-01-11 16:26:39 +0100

    account profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -13,7 +13,7 @@ async function Page() {
         username: userInfo?.username || user?.username,
         name: userInfo?.name || user?.firstName || "",
         bio: userInfo?.bio || "",
-        image: userInfo?.image : user?.imageUrl,
+        image: userInfo?.image || user?.imageUrl,
     }
 
     return (
