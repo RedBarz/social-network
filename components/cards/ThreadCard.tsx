0d68ecbfbc8a54ab5789e13485f42ec6b11c5612
RedBarz <barzcorp.fx@gmail.com>
2024-01-18 18:56:02 +0100

    add details

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { formatDateString } from "@/lib/utils";
 
 interface Props {
     id: string,
@@ -86,16 +87,35 @@ const ThreadCard = ({ id, currentUserId, parentId, content, author, community, c
                                     className="cursor-pointer object-contain"
                                 />
 
-                                {isComment && comments.length > 0 && (
-                                    <Link href={`/thread/${id}`}>
-                                        <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
-                                    </Link>
-                                )}
                             </div>
+                            {isComment && comments.length > 0 && (
+                                <Link href={`/thread/${id}`}>
+                                    <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                                </Link>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
+            {!isComment && community && (
+                <Link
+                    href={`/communities/${community.id}`}
+                    className='mt-5 flex items-center'
+                >
+                    <p className='text-subtle-medium text-gray-1'>
+                        {formatDateString(createdAt)}
+                        {community && ` - ${community.name} Community`}
+                    </p>
+
+                    <Image
+                        src={community.image}
+                        alt={community.name}
+                        width={14}
+                        height={14}
+                        className='ml-1 rounded-full object-cover'
+                    />
+                </Link>
+            )}
         </article>
     )
 }
